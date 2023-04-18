9a05ecc3f66206e44ca3ab6ec6251995aa999c38
matheus7k <06matheus.oliveira@gmail.com>
2023-04-18 00:01:22 -0300

    CRUD Cidade

--- Turismo/Program.cs
+++ Turismo/Program.cs
@@ -5,20 +5,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        #region CRUD CIDADE
+        /*
         Cidade cidade = new()
         {
             Descricao = "Araraquara",
             DataCadastro = DateTime.Now,
         };
-
+        */
         //new CidadeController().Insert(cidade);
 
         //new CidadeController().UpdateCidade("Adonai", "Shalom");
 
-        new CidadeController().GetCidades().ForEach(Console.WriteLine);
+        //new CidadeController().GetCidades().ForEach(Console.WriteLine);
 
         //new CidadeController().Delete("Guaribas");
-        
+        #endregion
+
+
     }
 }
